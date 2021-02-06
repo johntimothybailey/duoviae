ee3e5f1cb19ff4868f15aa423d66f09c91ec9c0c
John Bailey <641367+johntimothybailey@users.noreply.github.com>
2021-02-05 15:13:48 -1000

    various fixes

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,9 @@
 module.exports = function (api) {
-  api.cache(true)
+  api.cache(false)
   return {
     presets: [
       'babel-preset-expo',
       '@babel/typescript'
-    ],
-    env: {
-      production: {
-        plugins: ['react-native-paper/babel']
-      }
-    }
+    ]
   }
 }
