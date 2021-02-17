7e5e6cd16e36c0e588af3fbcd3330bee41eeb76d
John Bailey <641367+johntimothybailey@users.noreply.github.com>
2021-02-17 12:35:40 -1000

    fix(dev): ESLint Saga installed in the correct location

--- babel.config.js
+++ babel.config.js
@@ -1,15 +1,11 @@
 module.exports = function (api) {
   api.cache(false)
   return {
-    extends: [
-      'plugin:redux-saga/recommended'
-    ],
     presets: [
       'babel-preset-expo',
       '@babel/typescript'
     ],
     plugins: [
-      'redux-saga',
       ['module:react-native-dotenv', {
         moduleName: '@env',
         path: '.env',
